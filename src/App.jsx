fc083ed759471ee011ea767eefb429a50fe29f20
kkameoo <khdg1202@naver.com>
2024-12-20 18:05:05 +0900

    setting

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Header from "./main_components/Header";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ src/main_components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <h2 class="text-center mb-4">
+      hoo
+      <img src="../images/ade.jpg" />
+    </h2>
+  );
+}
+export default Header;
